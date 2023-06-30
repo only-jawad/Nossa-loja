b207e7d662da380a2a419ccd1ba7155eafa9565a
Rafael Soster <rafazsmine517@gmail.com>
2023-06-29 22:18:24 -0300

    Atuzalização apresentação

--- cadastrar.php
+++ cadastrar.php
@@ -21,22 +21,46 @@
         // validação
 
         if (!$nome) {
-            $msgErro = "Informe o nome!";
+            $msgErro = '<div class="alert alert-danger d-flex align-items-center" role="alert">
+              <div>
+                  Informe o nome!
+              </div>
+            </div>';
         }
         if (!$nomeTime) {
-            $msgErro = $msgErro."<br> Informe o time!";
+            $msgErro .= '<div class="alert alert-danger d-flex align-items-center" role="alert">
+              <div>
+                  Informe o time!
+              </div>
+            </div>';
         }
         if (!$funcao) {
-            $msgErro = $msgErro."<br> Informe a funcao!";
+            $msgErro .= '<div class="alert alert-danger d-flex align-items-center" role="alert">
+              <div>
+                  Informe a função!
+              </div>
+            </div>';
         }
         if (!$sexo) {
-            $msgErro = $msgErro."<br> Informe o sexo!";
+            $msgErro .= '<div class="alert alert-danger d-flex align-items-center" role="alert">
+              <div>
+                  Informe o sexo!
+              </div>
+            </div>';
         }
         if (!$idade) {
-            $msgErro = $msgErro."<br> Informe a idade!";
+            $msgErro .= '<div class="alert alert-danger d-flex align-items-center" role="alert">
+              <div>
+              Informe a idade!
+              </div>
+            </div>';
         }
         if (!$link_img) {
-            $msgErro = $msgErro."<br> Informe o link!";
+            $msgErro .= '<div class="alert alert-danger d-flex align-items-center" role="alert">
+              <div>
+                  Informe o link!
+              </div>
+            </div>';
         }
         else {
             $stmt = $conn->prepare($sql);
@@ -93,77 +117,116 @@
           </div>
         </nav>
     </header>
-    <main>
-        <form action="" method="post">
-            Nome: <input type="text" name="nome" placeholder="Informe o nome"> <br><br>
-            Time: <input type="text" name="nomeTime" placeholder="Informe o time"> <br><br>
-            Função: <input type="text" name="funcao" placeholder="Informe a função"> <br><br>
-            Sexo: <select name="sexo">
-                <option value="">--Selecione o sexo do jogador--</option>
-                <option value="M">Masculino</option>
-                <option value="F">Feminino</option>
-            </select>
-            <br><br>
-            Idade: <input type="number" name="idade" placeholder="Informe a idade"><br><br>
-            Link imagem: <input type="text" name="link_img" placeholder="Informe o link"><br><br>
-            <div id="msg" style="color : red;"><?= $msgErro ?></div>
-            <br><br>
-            <button type="submit">Cadastrar</button>
-            <input type="hidden" value="1" name="submetido">
-        </form>
-        <h3>Listagem de Jogadores</h3>
-        <?php 
-            if (isset($_GET["buscar"])) {
-                $sql = "SELECT * FROM jogadores WHERE nome like '%".$_GET["buscar"]."%'";
-            }
-            else {
-                $sql = "SELECT * FROM jogadores";    
-            }
-            // $sql = "SELECT * FROM jogadores";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute(); 
-            $result = $stmt->fetchAll(); 
-        ?>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <td scope="col">ID</td>
-                    <td scope="col">Nome</td>
-                    <td scope="col">Time</td>
-                    <td scope="col">Função</td>
-                    <td scope="col">Sexo</td>
-                    <td scope="col">Idade</td>
-                    <td scope="col">Link IMG</td>
-                    <td scope="col"></td>
-                </tr>
-            </thead>
-            <?php foreach ($result as $reg): ?>
-                <tbody>
-                    <tr>
-                        <td scope="row"><?php echo $reg['id']?></td>
-                        <td><?php echo $reg['nome']?></td>
-                        <td><?php echo $reg['nomeTime']?></td>
-                        <td><?php echo $reg['funcao']?></td>
-                        <td><?php
-                                switch ($reg['sexo'] ) {
-                                    case 'M':
-                                        echo "Masculino";
-                                        break;
-                                    case 'F':
-                                        echo "Feminino";
-                                        break;
-                                }
-                        
-                            ?>
-                        </td>
-                        <td><?= $reg['idade'] ?></td>
-                        <td><?= $reg['link_img'] ?></td>
-                        <td><a href="jogadores_del.php?id=<?= $reg['id'] ?>" onclick=" return confirm('Confirma a exclusão?')">
-                        Exluir</a></td>
-                    </tr>
-                </tbody>
-            <?php endforeach; ?>
-        </table>
+    <main style="background-color: #FF770D;">
+        <br>
+        <div class="container" style="background-color: white; border-radius: 30px;">
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <form action="" method="post">
+                        <br>
+                        <h2 class="fw-bold mb-2 text-uppercase" style="color :#FF770D;">Cadastrar Jogadores</h2>
+                        <div class="form-group">
+                            <label for="">Nome:</label>
+                            <input type="text" name="nome" placeholder="Informe o nome" class="form-control"> <br>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Time: </label>
+                            <input type="text" name="nomeTime" placeholder="Informe o time" class="form-control"> <br>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Função:</label>
+                            <input type="text" name="funcao" placeholder="Informe a função" class="form-control"> <br>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Idade:</label>
+                            <input type="text" name="idade" placeholder="Informe a idade" class="form-control"> <br>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Sexo: </label>
+                            <select name="sexo" class="form-control form-select">
+                               <option value="" selected>--Selecione o sexo do jogador--</option>
+                               <option value="M">Masculino</option>
+                               <option value="F">Feminino</option>
+                           </select>
+                           <br><br>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Link imagem:</label>
+                            <input type="text" name="link_img" placeholder="Informe o link" class="form-control"> <br>
+                        </div>
+                        <div id="msg"><?= $msgErro ?></div>
+                        <br><br>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary btn-lg main-btn">Cadastrar</button>
+                            <input type="hidden" value="1" name="submetido">
+                            <br><br>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <h3>Listagem de Jogadores</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <?php 
+                        if (isset($_GET["buscar"])) {
+                            $sql = "SELECT * FROM jogadores WHERE nome like '%".$_GET["buscar"]."%'";
+                        }
+                        else {
+                            $sql = "SELECT * FROM jogadores";    
+                        }
+                        // $sql = "SELECT * FROM jogadores";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->execute(); 
+                        $result = $stmt->fetchAll(); 
+                    ?>
+                    <table class="table table-striped table-bordered border-warning">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Time</th>
+                                <th scope="col">Função</th>
+                                <th scope="col">Sexo</th>
+                                <th scope="col">Idade</th>
+                                <th scope="col">Link IMG</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <?php foreach ($result as $reg): ?>
+                            <tbody>
+                                <tr>
+                                    <td scope="row"><?php echo $reg['id']?></td>
+                                    <td><?php echo $reg['nome']?></td>
+                                    <td><?php echo $reg['nomeTime']?></td>
+                                    <td><?php echo $reg['funcao']?></td>
+                                    <td><?php
+                                            switch ($reg['sexo'] ) {
+                                                case 'M':
+                                                    echo "Masculino";
+                                                    break;
+                                                case 'F':
+                                                    echo "Feminino";
+                                                    break;
+                                            }
+                                    
+                                        ?>
+                                    </td>
+                                    <td><?= $reg['idade'] ?></td>
+                                    <td><?= $reg['link_img'] ?></td>
+                                    <td><a href="jogadores_del.php?id=<?= $reg['id'] ?>" onclick=" return confirm('Confirma a exclusão?')" class="excluir">
+                                    Exluir</a></td>
+                                </tr>
+                            </tbody>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <br>
     </main>
     <footer></footer>
 </body>
